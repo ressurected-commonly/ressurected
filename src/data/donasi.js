c7f56e446d1055bae3812806f1cdab398ffeda4b
restu aji pratama <restuajipatama@gmail.com>
2022-05-21 09:15:45 +0700

    convert HTML to svelte components

---
+++ src/data/donasi.js
@@ -0,0 +1,16 @@
+module.export = {
+    charities: [
+        {
+            id: 1,
+            title: 'first donation project',
+            thumbnail: 'https://www.flickr.com/photos/142882734@N07/52077601310/in/explore-2022-05-17/',
+            pledged: 0,
+            target:10000000,
+            date_end: new Date('25 May 2022'),
+            profile_photo: 'https://www.flickr.com/photos/87281428@N04/52074919002/in/explore-2022-05-17/',
+            profile_name: 'Restu Aji',
+            no_pledges: 0
+        }
+    ]
+
+}
---
+++ src/pages/Home.svelte
@@ -0,0 +1,15 @@
+<script>
+	import Donasicharity from "Donasicharity.svelte";
+	import Header from '/Header.svelte';
+	import Welcome from '/Welcome.svelte';
+	import Promo from 'Promo.svelte';
+	import Footer from 'Footer.svelte';
+
+	</script>
+
+ 
+ <Header />
+ <Welcome />
+ <Donasicharity />
+ <Promo />
+ <Footer />
